7ee6a1d8831be8cb84c29f80b2041c83165f58a2
Thienvo2124 <siren11229@gmail.com>
2025-08-06 12:00:10 +0700

    .

---
+++ webtruyen/Home/js/truyen-data.js
@@ -0,0 +1,38 @@
+const danhSachTruyen = [
+  {
+    ten: "Tao Muốn Trở Thành Chúa Tể Bóng Tối!!",
+    chuong: "1",
+    link: "/webtruyen/Home/info_truyen/truyen-Tao Muốn Trở Thành Chúa Tể Bóng Tối!!.html",
+    img: "/webtruyen/Home/img/img_truyen/Tao Muốn Trở Thành Chúa Tể Bóng Tối!!/tao-muon-tro-thanh-chua-te-bong-toi_1546992987.jpg"
+  },
+  {
+    ten: "Gacha Vô Hạn",
+    chuong: "2",
+    link: "/webtruyen/Home/info_truyen/truyen-Gacha Vô Hạn.html",
+    img: "/webtruyen/Home/img/img_truyen/Gacha Vô Hạn/gacha-vo-han_1672285600.jpg"
+  },
+  {
+    ten: "Hôm Nay - Tôi Hóa Kaiju",
+    chuong: "10",
+    link: "/webtruyen/Home/info_truyen/truyen-Hôm Nay - Tôi Hóa Kaiju.html",
+    img: "/webtruyen/Home/img/img_truyen/Hôm Nay - Tôi Hóa Kaiju/hom-nay-toi-hoa-kaiju_1595055488.jpg"
+  },
+  {
+    ten: "Gachi Akuta",
+    chuong: "10",
+    link: "/webtruyen/Home/info_truyen/truyen-Gachi Akuta.html",
+    img: "/webtruyen/Home/img/img_truyen/Gachi Akuta/gachiakuta_1663074350.jpg"
+  },
+  {
+    ten: "Lúc Đó, Tôi Đã Chuyển Sinh Thành Slime",
+    chuong: "1",
+    link: "/webtruyen/Home/info_truyen/truyen-Lúc Đó, Tôi Đã Chuyển Sinh Thành Slime.html",
+    img: "/webtruyen/Home/img/img_truyen/Lúc Đó, Tôi Đã Chuyển Sinh Thành Slime/tensei-shitara-slime-datta-ken_1447076423.jpg"
+  },
+  {
+    ten: "Rebuild World",
+    chuong: "1",
+    link: "/webtruyen/Home/info_truyen/truyen-Rebuild World.html",
+    img: "/webtruyen/Home/img/img_truyen/Rebuild World/rebuild-world_1565070595.jpg"
+  }
+];
